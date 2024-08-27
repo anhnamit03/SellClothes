abe515ae5794cb2b37e68fc69c7d0bb69eac343d
anhnamit03 <nam001202030355@gmail.com>
2024-08-27 12:55:10 +0700

    first commit

---
+++ WebBanHangOnline/App_Start/AppSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace WebBanHangOnline.App_Start
+{
+    public class AppSettings
+    {
+        // Các thiết lập của ứng dụng
+        public const string GoogleRecaptchaSecretKey = "6LfNcakpAAAAACoKz2oeF-M6hf562r6Yj3QdYXzU";
+        public const string GoogleRecaptchaSiteKey = "6LfNcakpAAAAADLM6LFYDyVbPnpltoqJrxoBZuj7";
+
+        // Các thiết lập khác có thể được thêm vào đây
+    }
+}
